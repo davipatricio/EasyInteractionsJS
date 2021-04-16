a3eb46ca2fed754638b53535c8698465f98283e2
davipatricio <71164752+davipatricio@users.noreply.github.com>
2021-04-16 16:30:11 -0300

    Create DenkyCommandExample.js

---
+++ DenkyCommandExample.js
@@ -0,0 +1,23 @@
+class ReportCommand {
+	constructor() {
+		return {
+			perm: {
+				bot: ['MANAGE_MESSAGES', 'EMBED_LINKS'],
+				user: [],
+				owner: false,
+			},
+			nome: 'denunciar',
+			cat: 'Denúncias',
+			desc: 'Denuncie um membro ao servidor.',
+			aliases: ['denúnciar', 'report', 'reportar', 'denuncia', 'denúncia'],
+			run: this.run,
+		};
+	}
+
+	async run(client, msg, args, prefixo, t, rawIdioma) {
+			msg.q('hello world')
+		}
+	}
+}
+
+module.exports = ReportCommand;
